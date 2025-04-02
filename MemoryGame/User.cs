b939be953fa652d64462cd99c0316eb0bf35cd0a
sergiu033 <sergiu.carpinisan.04@gmail.com>
2025-04-02 10:22:57 +0300

    Signup page halfway done

---
+++ MemoryGame/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string userName;
+        private string profilePictureFilePath;
+
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                if (userName != value)
+                {
+                    userName = value;
+                    OnPropertyChanged(nameof(UserName));
+                }
+            }
+        }
+
+        public string ProfilePictureFilePath
+        {
+            get { return profilePictureFilePath; }
+            set
+            {
+                if (profilePictureFilePath != value)
+                {
+                    profilePictureFilePath = value;
+                    OnPropertyChanged(nameof(ProfilePictureFilePath));
+                }
+            }
+        }
+
+        public User(string userName, string profilePictureFilePath)
+        {
+            UserName = userName;
+            ProfilePictureFilePath = profilePictureFilePath;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MemoryGame/ViewModels/SignInViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MemoryGame.ViewModels
+{
+    public class SignInViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<User> Users { get; set; }
+
+        private User selectedUser;
+        public User SelectedUser
+        {
+            get { return selectedUser; }
+            set
+            {
+                if (selectedUser != value)
+                {
+                    selectedUser = value;
+                    OnPropertyChanged(nameof(SelectedUser));
+                }
+            }
+        }
+
+        public SignInViewModel()
+        {
+            Users = new ObservableCollection<User>
+            {
+                new User("Alice", "ProfileImages/image1.jpg"),
+                new User("Bob", "ProfileImages/image2.jpg"),
+                new User("Charlie", "ProfileImages/image3.jpg")
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
